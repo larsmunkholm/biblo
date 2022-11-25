83d3d12a424f245d0d18dd28dfbbff278bf8456f
Lars (Mac) <lars@saxofonsolo.dk>
2022-11-26 00:47:23 +0100

    fix: change how `metroRequire.d.ts` is used and exported

--- packages/react-native/src/index.tsx
+++ packages/react-native/src/index.tsx
@@ -1,4 +1,3 @@
-import "./interfaces/metroRequire";
 export type { BibloBio, BibloItem } from "./interfaces/Biblo.interface";
 export * from "./interfaces/IndexProps.interface";
 export * from "./interfaces/ReaderProps.interface";
@@ -6,10 +5,17 @@ export { BibloProvider } from "./hooks/Biblo.hook";
 export { IndexScreen as BibloIndex } from "./screens/Index.screen";
 export { ReaderScreen as BibloReader } from "./screens/Reader.screen";
 
-export const bibloImporter = (context: __MetroModuleApi.RequireContext) =>
-    context
-        .keys()
-        .reduce((obj, key) => ({ ...obj, [key]: context(key) }), {}) as Record<
-        string,
-        any
-    >;
+export const bibloImporter = (context: {
+    keys(): string[];
+    (id: string): any;
+    <T>(id: string): T;
+    resolve(id: string): string;
+    id: string;
+}) =>
+    context.keys().reduce(
+        (obj: Record<string, any>, key: string) => ({
+            ...obj,
+            [key]: context(key),
+        }),
+        {},
+    ) as Record<string, any>;
