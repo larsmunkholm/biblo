30b96a8723273903eff744b2770c4bb2c28326bb
Lars (Mac) <lars@saxofonsolo.dk>
2022-11-27 17:26:37 +0100

    remove lib files

--- packages/addons/simple-markup/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { BibloAddon } from "@biblo/react-native";
-interface Options {
-    bold?: string;
-    italic?: string;
-}
-declare const _default: (options: Options) => BibloAddon;
-export default _default;
