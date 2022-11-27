6c3b2c8acf120a4bb0eb20a8c562ad6ef245d00d
Lars (Mac) <lars@saxofonsolo.dk>
2022-11-27 19:22:28 +0100

    fix: fix support for Expo Web

--- packages/react-native/scripts/copy-d-ts.js
+++ packages/react-native/scripts/copy-d-ts.js
@@ -3,7 +3,10 @@ const fs = require("fs");
 const path = require("path");
 
 const src = path.resolve(__dirname, "../src/metroRequire.d.ts");
-const dest = path.resolve(__dirname, "../lib/interfaces/metroRequire.d.ts");
+const dest = path.resolve(
+    __dirname,
+    "../lib/typescript/interfaces/metroRequire.d.ts",
+);
 
 fs.copyFile(src, dest, fs.constants.COPYFILE_FICLONE, () =>
     console.log("Copied metroRequire.d.ts successfully!"),
