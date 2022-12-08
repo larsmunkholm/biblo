6172723e18d35f10d687e7437d5c735c4125c467
Lars (Mac) <lars@saxofonsolo.dk>
2022-12-08 10:28:20 +0100

    feat: add error boundaries to Biblo index

--- packages/react-native/src/components/IndexSectionHeader.component.tsx
+++ packages/react-native/src/components/IndexSectionHeader.component.tsx
@@ -4,6 +4,7 @@ import { IndexOptions } from "../interfaces/IndexOptions.interface";
 import { useBiblo } from "../hooks/Biblo.hook";
 import { Typography } from "./Typography.component";
 import { getTextStyles, getViewStyles } from "../helpers/getStyles.helper";
+import { ErrorBoundary } from "./ErrorBoundary.component";
 
 interface Props {
     title: string;
@@ -24,38 +25,40 @@ export const IndexSectionHeader = ({ title, itemsCount }: Props) => {
         View) as NonNullable<IndexOptions["sectionHeaderComponent"]>;
 
     return itemsCount ? (
-        <Container
-            title={title}
-            style={getViewStyles(
-                [
-                    { height: indexOptions.sectionListItemHeight },
-                    indexOptions.sectionHeaderStyle,
-                ],
-                {
-                    paddingHorizontal: defaultStyles.margin,
-                    backgroundColor: "#ccc",
-                    justifyContent: "center",
-                },
-                disableDefaultStyles,
-            )}
-            textStyle={indexOptions.sectionHeaderTextStyle}
-        >
-            <Typography
-                bold
-                disableDefaultStyles={disableDefaultStyles}
-                style={indexOptions.sectionHeaderTextStyle}
+        <ErrorBoundary type="sectionHeaderComponent">
+            <Container
+                title={title}
+                style={getViewStyles(
+                    [
+                        { height: indexOptions.sectionListItemHeight },
+                        indexOptions.sectionHeaderStyle,
+                    ],
+                    {
+                        paddingHorizontal: defaultStyles.margin,
+                        backgroundColor: "#ccc",
+                        justifyContent: "center",
+                    },
+                    disableDefaultStyles,
+                )}
+                textStyle={indexOptions.sectionHeaderTextStyle}
             >
-                {textParser(title, {
-                    type: "title",
-                    screen: "index",
-                    scope: "section",
-                    style: getTextStyles(
-                        indexOptions.sectionHeaderTextStyle,
-                        { fontWeight: "bold" },
-                        disableDefaultStyles,
-                    ),
-                })}
-            </Typography>
-        </Container>
+                <Typography
+                    bold
+                    disableDefaultStyles={disableDefaultStyles}
+                    style={indexOptions.sectionHeaderTextStyle}
+                >
+                    {textParser(title, {
+                        type: "title",
+                        screen: "index",
+                        scope: "section",
+                        style: getTextStyles(
+                            indexOptions.sectionHeaderTextStyle,
+                            { fontWeight: "bold" },
+                            disableDefaultStyles,
+                        ),
+                    })}
+                </Typography>
+            </Container>
+        </ErrorBoundary>
     ) : null;
 };
